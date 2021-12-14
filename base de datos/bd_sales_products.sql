f50fd2e65791f64bc898ee7f6f4fff2de031182d
juniorjaviersanchez <juniorsc750@gmail.com>
2021-12-14 14:47:52 -0500

    update bd

--- base de datos/bd_sales_products.sql
+++ base de datos/bd_sales_products.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2021 a las 22:40:18
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
+-- Servidor: 127.0.0.1:3309
+-- Tiempo de generación: 14-12-2021 a las 19:41:21
+-- Versión del servidor: 5.7.31
+-- Versión de PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,45 +27,29 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
-  `route` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `route` varchar(255) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`, `description`, `date`, `route`) VALUES
-(48, 'Computadora', '222.00', 'Computas', '2021-12-10 00:00:00', 'compu.jpg'),
-(49, 'WEE', '323.00', 'dsd', '2021-12-10 00:00:00', 'firma.jpg'),
-(50, 'Computadora 123', '333.00', 'EEE', '2021-12-10 00:00:00', '46993311_1084110435083094_6117065733483528192_n.jpg'),
-(57, 'SSS', '34.00', 'asd', '2021-12-10 00:00:00', 'perifl.png'),
-(58, 'SS', '100.00', 'ZZZ', '2021-12-10 00:00:00', 'compu.jpg');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `date`, `route`, `cantidad`) VALUES
+(48, 'Computadora', '100.00', 'Computas', '2021-12-10 00:00:00', 'compu.jpg', 10),
+(49, 'WEE', '323.00', 'dsd', '2021-12-10 00:00:00', 'firma.jpg', 0),
+(50, 'Computadora 123', '333.00', 'EEE', '2021-12-10 00:00:00', '46993311_1084110435083094_6117065733483528192_n.jpg', 0),
+(57, 'SSS', '34.00', 'asd', '2021-12-10 00:00:00', 'perifl.png', 19),
+(58, 'SS', '100.00', 'ZZZ', '2021-12-10 00:00:00', 'compu.jpg', 0),
+(62, 'Ejemplo', '23.00', 'er', '2021-12-14 00:00:00', 'WhatsApp Image 2021-10-10 at 11.17.01 AM (1).jpeg', 18);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
